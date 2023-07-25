112cd98faccb90d22780e53151e447a70fec156d
Dockins207 <129792373+Dockins207@users.noreply.github.com>
2023-07-25 16:19:19 +0300

    printf

--- main.h
+++ main.h
@@ -41,7 +41,7 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+_va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
--- write_handlers.c
+++ write_handlers.c
@@ -95,7 +95,7 @@ int write_num(int ind, char buffer[],
 	int flags, int width, int prec,
 	int length, char padd, char extra_c)
 {
-	int i, padd_start = 1;
+	int f, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
 		return (0); /* printf(".0d", 0)  no char is printed */
